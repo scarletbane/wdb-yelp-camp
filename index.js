f1630a22d364c735675f9d9dde0176923b07a087
Matthew Rose <47019415+scarletbane@users.noreply.github.com>
2023-02-24 15:38:33 -0700

    add css

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 const port = 3000;
 
 app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.render('index');
