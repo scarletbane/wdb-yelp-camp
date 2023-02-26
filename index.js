39bafb0a55bc4e632789f955d73adcd1412b818d
Matthew Rose <47019415+scarletbane@users.noreply.github.com>
2023-02-26 05:50:43 -0700

    move head into its own partial and add a pages dir

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('pages/index');
 });
 
 app.listen(port, () => {
