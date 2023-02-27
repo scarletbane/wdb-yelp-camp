d5f366ca693e4d702502363672952216bb91a058
Matthew Rose <47019415+scarletbane@users.noreply.github.com>
2023-02-27 13:22:10 -0700

    add model and create basic route to test adding

--- app.js
+++ app.js
@@ -1,35 +1,50 @@
-const express = require('express');
-const path = require('path');
-const ejsMate = require('ejs-mate');
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
+const express = require("express");
+const path = require("path");
+const ejsMate = require("ejs-mate");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const Campground = require("./models/campground");
 
 dotenv.config();
 
-mongoose.set('strictQuery', true);
+mongoose.set("strictQuery", true);
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING);
 
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.on('open', () => {
-    console.log('Database connected');
+db.on("error", console.error.bind(console, "connection error:"));
+db.on("open", () => {
+    console.log("Database connected");
 });
 
 const app = express();
 const port = 3000;
 
-app.engine('ejs', ejsMate);
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.use('/js', express.static(path.join(__dirname, '/node_modules/@popperjs/core/dist/umd')));
-app.use(express.static(path.join(__dirname, '/node_modules/bootstrap/dist')));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+    "/js",
+    express.static(
+        path.join(__dirname, "/node_modules/@popperjs/core/dist/umd")
+    )
+);
+app.use(express.static(path.join(__dirname, "/node_modules/bootstrap/dist")));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', (req, res) => {
-    res.render('pages/home', {title: 'Home'});
+app.get("/", (req, res) => {
+    res.render("pages/home", { title: "Home" });
+});
+
+app.get("/newcampground", async (req, res) => {
+    const camp = new Campground({
+        title: "My Backyard",
+    });
+    await camp.save();
 });
 
 app.listen(port, () => {
-    console.log(`App listening on port ${port}. Click the link http://localhost:${port}.`)
+    console.log(
+        `App listening on port ${port}. Click the link http://localhost:${port}.`
+    );
 });
