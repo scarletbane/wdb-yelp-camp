3740f4b420e2897ac4635465d0aeb9b2854e59f0
Matthew Rose <47019415+scarletbane@users.noreply.github.com>
2023-03-01 13:39:49 -0700

    add images to seed file

--- seeds/index.js
+++ seeds/index.js
@@ -27,16 +27,10 @@ const seedDB = async () => {
             author: "5f5c330c2cd79d538f2c66d9",
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
+            image: "https://source.unsplash.com/collection/483251",
             description:
                 "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!",
             price,
-            geometry: {
-                type: "Point",
-                coordinates: [
-                    cities[random1000].longitude,
-                    cities[random1000].latitude,
-                ],
-            },
         });
         await camp.save();
     }
